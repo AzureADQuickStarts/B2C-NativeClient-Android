0fb8e3d88ee8f6b8de5777e5793a24de80d3c6b3
Brandon Werner <brandwe@microsoft.com>
2015-09-19 13:46:45 -0700

    removing for skeleton

--- TaskApplication-AndroidStudio/TaskApplication/app/src/main/java/com/microsoft/aad/taskapplication/AddTaskActivity.java
+++ TaskApplication-AndroidStudio/TaskApplication/app/src/main/java/com/microsoft/aad/taskapplication/AddTaskActivity.java
@@ -11,31 +11,5 @@ import com.microsoft.aad.taskapplication.helpers.TodoListHttpService;
 
 public class AddTaskActivity extends Activity {
 
-    EditText textField;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
-        textField = (EditText) findViewById(R.id.taskToAdd);
-        Button button = (Button) findViewById(R.id.postTaskbutton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (textField.getText().toString() != null
-                        && !textField.getText().toString().trim().isEmpty()
-                        && Constants.CURRENT_RESULT != null) {
-
-                    TodoListHttpService service = new TodoListHttpService();
-                    try {
-                        service.addItem(textField.getText().toString(), Constants.CURRENT_RESULT.getAccessToken());
-                    } catch (Exception e) {
-                        SimpleAlertDialog.showAlertDialog(getApplicationContext(), "Exception caught", e.getMessage());
-                    }
-                    finish();
-                }
-            }
-        });
-    }
 
 }
